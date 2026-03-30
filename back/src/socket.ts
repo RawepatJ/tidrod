6cb519af0dce2e74f67265045de4c457c926eb0a
Squarewagon <natchanon.0307@gmail.com>
2026-03-30 16:32:34 +0700

    test commit

--- back/src/socket.ts
+++ back/src/socket.ts
@@ -32,6 +32,7 @@ export function setupSocket(httpServer: HttpServer): Server {
             next(new Error('Invalid token'));
         }
     });
+    
 
     io.on('connection', (socket) => {
         const user = (socket as any).user;
